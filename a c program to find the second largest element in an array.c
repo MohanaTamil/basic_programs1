0a19fb645d4ec23ff1e84e6e44095a96c047953c
Mohana Tamil <77664807+MohanaTamil@users.noreply.github.com>
2021-03-19 14:31:59 +0530

    Add files via upload

--- a c program to find the second largest element in an array.c
+++ a c program to find the second largest element in an array.c
@@ -4,7 +4,8 @@ int main()
 	int a[100],i,first_large,second_large,temp,n;
 	printf("Input the size of array: ");
 	scanf("%d",&n);
-	printf("Input 5 elements in an array\n");
+	printf("input 5 elements in an array\n");
+	
 	for(i=0;i<n;i++)
 	 {
 	    printf("element-%d: ",i);
